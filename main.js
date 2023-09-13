f69cbcd26d06d8342abeab8423becfeaa483dab4
Alla <allanudelman@gmail.com>
2023-09-13 23:50:44 +0300

    added css

--- main.js
+++ main.js
@@ -19,6 +19,34 @@ if(navClose){
   })
 }
 
+/* ===================== SCROLL SECTION ACTIVE LINK ===================== */
+
+const sections = document.querySelectorAll('section[id]')
+function scrollActive(){
+  const scrollY = window.pageYOffset
+  sections.forEach(current=>{
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop-50;
+    sectionId = current.getAttribute('id')
+    if(scrollY>sectionTop&&scrollY<=sectionTop+sectionHeight){
+      document.querySelector('.nav-menu a[href*='+ sectionId +']').classList.add('active-link')}
+    else{
+      document.querySelector('.nav-menu a[href*='+ sectionId +']').classList.remove('active-link')
+      }
+
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/* ===================== CHANGE BACKGROUND HEADER ===================== */
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  if(this.scrollY>=80)nav.classList.add('scroll-header');else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
 /* ===================== REMOVE MENU MOBILE ===================== */
 
 // Select all elements with class .nav-link
