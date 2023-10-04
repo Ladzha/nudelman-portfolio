653cc295930afc3a88449875c75538fa6114da81
Alla <allanudelman@gmail.com>
2023-10-04 20:49:16 +0300

    added video

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ window.addEventListener('scroll', scrollTop)
 /* ================== SHOW SCROLL UP ================== */
 function scrollUp(){
   const scrollUp = document.getElementById('scroll-up')
-  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');else scrollUp.classList.remove('show-scroll')
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 
 window.addEventListener('scroll', scrollUp)
